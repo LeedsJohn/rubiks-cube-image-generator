9d67eca7a38208da3f206e8a2d41592cd441e429
John Leeds <leedsjohn6@gmail.com>
2022-07-05 15:34:03 -0500

    Adjusted file path

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ John Leeds
 Driver program to create images for my algorithm trainer
 """
 import json
-import imagegenerator
+import three_image_gen
 
 def main():
-    # algSets = ["WVLS"]
-    algSets = ["CMLL", "COLL", "ELL", "OLL", "PLL", "WVLS_FL", "WVLS", "ZBLL_AS",
+    three = ["CMLL", "COLL", "ELL", "OLL", "PLL", "WVLS_FL", "WVLS", "ZBLL_AS",
                "ZBLL_H", "ZBLL_L", "ZBLL_Pi", "ZBLL_S", "ZBLL_T", "ZBLL_U"]
-    for algSet in algSets:
+    for algSet in three:
         states = {}
-        with open(f"input/{algSet}.json") as f:
+        with open(f"input/three/{algSet}.json") as f:
             states = json.load(f)
         for alg in states:
             print(f"states[alg]: {states[alg]} - alg: {alg} - algSet: {algSet}")
-            gen = imagegenerator.ImageGen(states[alg][0], alg.replace(" ", "_"), algSet)
+            gen = three_image_gen.ImageGen(states[alg][0], alg.replace(" ", "_"), algSet)
             gen.make()
 
 if __name__ == "__main__":
