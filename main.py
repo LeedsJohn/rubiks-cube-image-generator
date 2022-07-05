eb6622718a7eb6f33331770f664b88cd24ecedc5
John Leeds <leedsjohn6@gmail.com>
2022-07-05 15:57:46 -0500

    add more two by two algsets

--- main.py
+++ main.py
@@ -7,18 +7,29 @@ Driver program to create images for my algorithm trainer
 """
 import json
 import three_image_gen
+import two_image_gen
 
 def main():
-    three = ["CMLL", "COLL", "ELL", "OLL", "PLL", "WVLS_FL", "WVLS", "ZBLL_AS",
-               "ZBLL_H", "ZBLL_L", "ZBLL_Pi", "ZBLL_S", "ZBLL_T", "ZBLL_U"]
-    for algSet in three:
+    two = ["CLL", "EG-1", "EG-2", "Ortega_OLL", "Ortega_PBL"]
+    for algSet in two:
         states = {}
-        with open(f"input/three/{algSet}.json") as f:
+        with open(f"input/two/{algSet}.json") as f:
             states = json.load(f)
         for alg in states:
             print(f"states[alg]: {states[alg]} - alg: {alg} - algSet: {algSet}")
-            gen = three_image_gen.ImageGen(states[alg][0], alg.replace(" ", "_"), algSet)
+            gen = two_image_gen.ImageGen(states[alg][0], alg.replace(" ", "_"), algSet)
             gen.make()
+
+    # three = ["CMLL", "COLL", "ELL", "OLL", "PLL", "WVLS_FL", "WVLS", "ZBLL_AS",
+    #            "ZBLL_H", "ZBLL_L", "ZBLL_Pi", "ZBLL_S", "ZBLL_T", "ZBLL_U"]
+    # for algSet in three:
+    #     states = {}
+    #     with open(f"input/three/{algSet}.json") as f:
+    #         states = json.load(f)
+    #     for alg in states:
+    #         print(f"states[alg]: {states[alg]} - alg: {alg} - algSet: {algSet}")
+    #         gen = three_image_gen.ImageGen(states[alg][0], alg.replace(" ", "_"), algSet)
+    #         gen.make()
 
 if __name__ == "__main__":
     main()
